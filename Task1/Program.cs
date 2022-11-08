3b80ae061d5138574174dc821c1f60f6b9347a57
Anatolii Voloshyn <sana22_vas@student.ztu.edu.ua>
2022-11-08 21:21:31 +0200

    2 last lines

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.WriteLine("1 line");
 Console.WriteLine("2 line");
 Console.WriteLine("3 line");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("4 line");
+Console.WriteLine("5 line");
