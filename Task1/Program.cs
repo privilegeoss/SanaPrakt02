f3379d71495d1d19c49354d52d3b5fb59e40013b
Anatolii Voloshyn <sana22_vas@student.ztu.edu.ua>
2022-11-08 21:20:00 +0200

    3 random lines

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,5 +1,5 @@
-﻿Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+﻿Console.WriteLine("1 line");
+Console.WriteLine("2 line");
+Console.WriteLine("3 line");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
